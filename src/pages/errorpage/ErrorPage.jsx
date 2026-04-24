a0a3085908e98b289d403cd1955ac8d8da17bd4f
Sabikun Nahar Farha <sabikunnaharfarha@Sabikuns-MacBook-Air.local>
2026-04-24 19:03:13 +0600

    errorpage modified

--- src/pages/errorpage/ErrorPage.jsx
+++ src/pages/errorpage/ErrorPage.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router"
 
 
 const ErrorPage=()=> {
   return (
-    <div>ErrorPage</div>
+    <div className="text-center my-20 space-y-4">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p>Page Not Found</p>
+      <Link to='/'><button className="btn bg-[#244D3F] text-white">Back to Home</button></Link>
+    </div>
   )
 }
 
